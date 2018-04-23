6a49e6692895a9dff72182c0211c13666798fd99
shiyicode <458241107@qq.com>
2018-04-24 02:20:51 +0800

    fix createtoken

--- common/components/jwt.go
+++ common/components/jwt.go
@@ -6,14 +6,15 @@ import (
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/shiyicode/gin-framework/common/g"
+	"strconv"
 )
 
 var mySigningKey = "let's to change the world!"
 
-func CreateToken(userId int) (string, error) {
+func CreateToken(userId int64) (string, error) {
 	cfg := g.Conf()
 	claims := jwt.MapClaims{
-		"uid": userId,
+		"uid": strconv.FormatInt(userId,10),
 		"exp": time.Now().Add(time.Hour * 24 * time.Duration(cfg.Jwt.MaxEffectiveTime)).Unix(),
 	}
 
--- router/controllers/base/user.go
+++ router/controllers/base/user.go
@@ -1,12 +1,15 @@
 package base
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"strconv"
+)
 
 func UserId(c *gin.Context) int64 {
-	id, isExit := c.Get("userId")
-	if isExit == true {
-		num := id.(int64)
-		return num
+	str, exists := c.Get("userId")
+	if exists == false {
+		return 0
 	}
-	return 0
+	id, _ := strconv.ParseInt(str.(string), 10, 64)
+	return id
 }
