54cf58507c2572cca04336767eb78cb1746a0568
师毅 <458241107@qq.com>
2018-04-24 22:11:18 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -26,6 +26,7 @@ function make_output {
 		cp -vrf $app $output &&       # 拷贝 二进制文件 至output目录
 		cp -vrf $cfg $output &&       # 拷贝 cfg配置文件目录 至output目录
 		cp -vrf control.sh $output && # 拷贝 启停脚本 至output目录
+		cp -vrf scripts $output &&    # 拷贝 脚本目录 至output目录
 		echo -e "make output ok"
 	) || { echo -e "make output error"; rm -rf "./output"; exit 2; }
 }
